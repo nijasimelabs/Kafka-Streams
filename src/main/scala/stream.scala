b90bb5dec7830d23f2dd1d8430d556c96f707cdb
Nija <nija@simelabs>
2018-08-09 08:10:20 +0000

    Join two streams

--- src/main/scala/stream.scala
+++ src/main/scala/stream.scala
@@ -1,11 +1,15 @@
 import java.util.Properties
-import org.apache.kafka.common.serialization._
-import org.apache.kafka.streams._
-import play.api.libs.json._
 import org.apache.kafka.connect.json.JsonDeserializer;
 import org.apache.kafka.connect.json.JsonSerializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import org.apache.kafka.streams.kstream.{KStream, KStreamBuilder, KTable,   ForeachAction}
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.streams.kstream.{Printed, KStream, KTable, Produced, Serialized}
+import org.apache.kafka.streams.kstream.ValueJoiner
+import org.apache.kafka.streams._
+
 
 
 /**
@@ -13,18 +17,7 @@ import org.apache.kafka.streams.kstream.{KStream, KStreamBuilder, KTable,   Fore
   */
 object ChannelGroupProfileStream {
   def main(args: Array[String]): Unit = {
-    val config = {
-      val properties = new Properties()
-      properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-application")
-      properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-      properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
-      properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
-      properties
-    }
     
-    val jsonSerializer: Serializer[JsonNode] = new JsonSerializer()
-    val jsonDeserializer: Deserializer[JsonNode] = new JsonDeserializer()
-    val jsonSerde: Serde[JsonNode] = Serdes.serdeFrom(jsonSerializer, jsonDeserializer)
     val wanOperationalDbTopic: String = "wan_op_db"
     val trafficShapingTopic: String ="traffic_shaping"
     val trafficClassTopic: String = "traffic_class"
@@ -32,28 +25,40 @@ object ChannelGroupProfileStream {
     val operationalSCPCTopic: String = "operational_scpc"
     val remoteTopic: String = "remote"
 
-    val stringSerde = Serdes.String()
-    val integerSerde = Serdes.Integer()
-    val builder = new KStreamBuilder() 
+    val stringSerde: Serde[String] = Serdes.String()
+    val jsonSerializer: Serializer[JsonNode] = new JsonSerializer()
+    val jsonDeserializer: Deserializer[JsonNode] = new JsonDeserializer()
+    val jsonSerde: Serde[JsonNode] = Serdes.serdeFrom(jsonSerializer, jsonDeserializer)
+
+
+    val config = {
+      val properties = new Properties()
+      properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-application")
+      properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+      properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+      properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+      properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+
+      properties
+    }
+
+    val builder: StreamsBuilder = new StreamsBuilder()
+
    
-    val trafficShapingStream: KStream[String, JsonNode] = builder.stream(stringSerde, jsonSerde, trafficShapingTopic);
+    val trafficShapingStream: KStream[String, JsonNode] = builder.stream(trafficShapingTopic, Consumed.`with`(stringSerde, jsonSerde));
 
-    val trafficClassificationStream: KStream[String, JsonNode] = builder.stream(stringSerde, jsonSerde, trafficClassificationTopic)
-    
-    trafficShapingStream.foreach(new ForeachAction[String, JsonNode]() {
-      override def apply(key: String, value: JsonNode): Unit = {
-        println(key + " 1: " + value.get("nodename"))
-      }
-    });
+    val trafficClassificationTable: KTable[String, JsonNode] = builder.table(trafficClassificationTopic, Consumed.`with`(stringSerde, jsonSerde))
 
-    trafficClassificationStream.foreach(new ForeachAction[String, JsonNode]() {
-      override def apply(key: String, value: JsonNode): Unit = {
-        println(key + " 2: " + value.get("nodename"))
-      }
-    })
+    val joiner: ValueJoiner[JsonNode, JsonNode, String] = new ValueJoiner[JsonNode, JsonNode, String]() {
+      def apply(a: JsonNode, b: JsonNode): String = a.path("nodename").asText()
+    }    
     
-    //val trafficClassificationStream: KStream[String, JsonNode]  = trafficShapingStream.join(trafficClassificationStream -> trafficClassificationStream.path("nodename"));
-    val  streams = new KafkaStreams(builder, config);
+    val joined: KStream[String, String]  = trafficShapingStream.leftJoin(trafficClassificationTable,  joiner)
+    
+
+    joined.to("out-topic", Produced.`with`(stringSerde, stringSerde))
+    
+    val streams: KafkaStreams = new KafkaStreams(builder.build(), config)
     streams.start();
 
     
