0c5c65b0461287ea892a2484631e7e2672362182
Nija <nija@simelabs.com>
2018-08-13 12:34:51 +0530

    fix variable initialization
    add code formatting

--- src/main/scala/TrafficStreamProcessing.scala
+++ src/main/scala/TrafficStreamProcessing.scala
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 object TrafficStreamProcessing {
   def main(args: Array[String]): Unit = {
-    
+
     val wanOperationalDbTopic: String = "wan_op_db"
     val trafficTopic: String = "traffic"
     val operationalSCPCTopic: String = "operational_scpc"
@@ -39,57 +39,53 @@ object TrafficStreamProcessing {
       properties
     }
 
-   val builder: StreamsBuilder = new StreamsBuilder()
-  val aggregate_values:ObjectNode = JsonNodeFactory.instance.objectNode();
+    val builder: StreamsBuilder = new StreamsBuilder()
+    val aggregate_values:ObjectNode = JsonNodeFactory.instance.objectNode();
 
 
+    //define stream here
+    val trafficStream: KStream[String, JsonNode] = builder.stream(trafficTopic, Consumed.`with`(stringSerde, jsonSerde));
+    val operatioanalStream: KTable[String, JsonNode] = builder.table(operationalSCPCTopic, Consumed.`with`(stringSerde, jsonSerde));
 
+    // processing the stream for aggregation
+    trafficStream.foreach(
+      new ForeachAction[String, JsonNode]() {
+        override def apply(key: String, value: JsonNode): Unit = {
 
-   //define stream here 
-   val trafficStream: KTable[String, JsonNode] = builder.table(trafficTopic, Consumed.`with`(stringSerde, jsonSerde));
-   val operatioanalStream: KTable[String, JsonNode] = builder.table(operationalSCPCTopic, Consumed.`with`(stringSerde, jsonSerde));
- 
-   // processing the stream for aggregation
-   trafficStream.foreach(new ForeachAction[String, JsonNode]() {
-      override def apply(key: String, value: JsonNode): Unit = {
-        
-         // calculate agrgegate cir, mir for each link
-        // cir should be the sum of cir and max of mir 
-
-          var max_mir:Double = 0.0;
-          var sum_cir:Double = 0.0;
-          val link:ObjectNode = JsonNodeFactory.instance.objectNode();
- 
           for( x <- 0 until value.size()){
 
-          var link_name = value.get(x).get("link").asText();
+            // calculate agrgegate cir, mir for each link
+            // cir should be the sum of cir and max of mir
+            var max_mir:Double = 0.0;
+            var sum_cir:Double = 0.0;
+            var link_name = value.get(x).get("link").asText();
+            val link:ObjectNode = JsonNodeFactory.instance.objectNode();
 
-           for ( i <-  0 until value.get(x).get("trafficclass").size()){
+            for ( i <-  0 until value.get(x).get("trafficclass").size()) {
 
-               val cir =  value.get(x).get("trafficclass").get(i).get("cir").asDouble();
-               val mir =  value.get(x).get("trafficclass").get(i).get("mir").asDouble()
-               sum_cir = sum_cir + cir
-               if (max_mir < mir){
-                  max_mir = mir;
-               }
-           }
-           // json node for cir mir aggregate
-           println("yea")
-           link.put("cir", sum_cir)
-           link.put("mir", max_mir)
-           aggregate_values.put(link_name, link)
-           println("asvasvf:     "+aggregate_values)
+              val cir =  value.get(x).get("trafficclass").get(i).get("cir").asDouble();
+              val mir =  value.get(x).get("trafficclass").get(i).get("mir").asDouble()
+              sum_cir = sum_cir + cir
+              if (max_mir < mir){
+                max_mir = mir;
+              }
+            }
+
+            // json node for cir mir aggregate
+            link.put("cir", sum_cir)
+            link.put("mir", max_mir)
+            aggregate_values.put(link_name, link)
+            println("asvasvf:     "+aggregate_values + "\n\n\n")
 
           }
         }
       });
 
-     
 
 
     val streams: KafkaStreams = new KafkaStreams(builder.build(), config)
     streams.start();
 
-    
+
   }
 }
